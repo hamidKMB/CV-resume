feddffb6ba350242409cea62789f2d94b0222aff
hamid kamyab <78806131+hamidKMB@users.noreply.github.com>
2021-04-05 17:22:19 +0430

    some New Feature and fix bugs
    fix bug : 
    change the way of clouds move on phone they won't move and on any screen bigger than 900 they will move.
    new feature:
    Greeting Feature

--- index.js
+++ index.js
@@ -15,26 +15,50 @@ var cloudBottom = $(".middle_cloud");
 var cloudTop = $(".top_cloud");
 //assign variables to Clouds
 
-// Stop when scrolls
+var mobile = $(window).width();
+console.log( "this is window width : " + mobile);
 
-$('body').ready(function(){  // when the body gets ready and loaded up
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop(); // also we could use window.pageYOffset instead $(window).scrollTop();
-    if (scroll < 100 ) { //we must change the 630 to 100 to fix the bug of mobile
-      console.log(scroll);
-        cloudBottom.animate({ left: "60px" }, 2500);
-        cloudTop.animate({ right: "3.75rem" }, 2500);
-        cloudBottom.animate({ left: "12.5rem" }, 2500);
-        cloudTop.animate({ right: "15rem" }, 3000);
-    } else{
-      cloudBottom.stop();
-      cloudTop.stop();
+function animation( value ){
+    if (value > 900 ) {
+    for( var i = 0 ; i < 100 ; i++ ){
+      middleCloud.animate({left:'60px'}, 2500);
+      topCloud.animate({right: '3.75rem'}, 2500);
+      middleCloud.animate({left: '12.5rem'}, 2500);
+      topCloud.animate({right: '15rem'}, 3000);
     }
-  });
-});
+  }
+}
+
+animation(mobile);
+
+
+// $('body').ready(function(){  // when the body gets ready and loaded up
+//   $(window).scroll(function () {
+//     var scroll = $(window).scrollTop(); // also we could use window.pageYOffset instead $(window).scrollTop();
+//     if (scroll < 100 ) { //we must change the 630 to 100 to fix the bug of mobile
+//       console.log(scroll);
+//         cloudBottom.animate({ left: "60px" }, 2500);
+//         cloudTop.animate({ right: "3.75rem" }, 2500);
+//         cloudBottom.animate({ left: "12.5rem" }, 2500);
+//         cloudTop.animate({ right: "15rem" }, 3000);
+//     } else{
+//       cloudBottom.stop();
+//       cloudTop.stop();
+//     }
+//   });
+// });
 
 
 // make clouds move when the window on them
+
+$('#font').delay(800).fadeOut(1500 ,function(){
+  $('#font').html("I'm Hamid.").fadeIn(1500 , function(){
+    $('#appear').fadeOut(10 , function(){
+      $('#appear').css('visibility','visible').fadeIn(800);
+    })
+  });
+});
+// Greeting feature
 
 
 //Get the button:
